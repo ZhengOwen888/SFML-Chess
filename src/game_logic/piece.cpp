31e67450cbd9af7a545378e947eae8dc6f397324
Owen Zheng <owenzheng922@gmail.com>
2025-10-23 23:22:26 -0400

    added comments to piece.cpp for implementation

---
+++ src/game_logic/piece.cpp
@@ -0,0 +1,36 @@
+#include "piece.hpp"
+
+namespace GameLogic
+{
+    // Implement constructor
+
+    // Implement destructor
+
+
+
+    // Check for "Pseudo-Legal" moves
+
+    // Implement getPseudoLegalMoves
+
+    // Implement isPseudoLegalMove
+
+
+
+    // Getters
+
+    // Implement getColor
+
+    // Implement getPieceType
+
+    // Implement hasMoved
+
+    // Implement hasPromoted
+
+
+
+    // Setters
+
+    // Implement setHasMoved
+
+    // Implement setHasPromoted
+}
