646e80b92f1c47aac05585c9ccfca17ccaa0ba00
Owen Zheng <owenzheng922@gmail.com>
2025-10-23 22:39:08 -0400

    added piece header, added a few type aliases in types.hpp

--- include/game_logic/types.hpp
+++ include/game_logic/types.hpp
@@ -1,9 +1,19 @@
 #ifndef GAMELOGIC_TYPES_HPP
 #define GAMELOGIC_TYPES_HPP
 
+#include <array>
+#include <utility>
+#include <memory>
+
 namespace GameLogic
 {
-    // global color enum in GameLogic namespace
+    // Board size (8x8)
+    constexpr int BOARD_SIZE = 8;
+
+    // Type alias for representating a position on board (row, col)
+    using Position = std::pair<int, int>;
+
+    // Enum class for color of chess pieces, players, and square on board
     enum class Color
     {
         DARK,
@@ -11,8 +21,23 @@ namespace GameLogic
         NONE
     };
 
-    // other globals or global enums to be deined later:
-    // PieceType, BOARD_SIZE, Direction ( there are 8 ), MoveType
+    // Enum for type of the chess pieces
+    enum class PieceType
+    {
+        // piece types here
+    };
+
+    // Enum for the direction a piece can move
+    enum class Direction
+    {
+        // directions here
+    };
+
+    // Enum for the type of the move
+    enum class MoveType
+    {
+        // move types here
+    };
 }
 
 #endif
